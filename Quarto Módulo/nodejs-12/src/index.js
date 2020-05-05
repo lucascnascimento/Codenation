413db6509fa32a2eadc7ad047c6ae363db2f0023
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-05 18:58:21 -0300

    Código final

--- Quarto Módulo/nodejs-12/src/index.js
+++ Quarto Módulo/nodejs-12/src/index.js
@@ -1,7 +1,98 @@
-const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+const promotions = ["SINGLE LOOK", "DOUBLE LOOK", "TRIPLE LOOK", "FULL LOOK"];
 
 function getShoppingCart(ids, productsList) {
-	return {};
+  let productsCart = getProductsFromCart(ids, productsList);
+  let products = getProducts(productsCart);
+  let promotion = setPromotion(productsCart);
+  let price = calculatePrice(productsCart, promotion);
+  let totalPrice = price.totalPrice.toFixed(2);
+  let discountValue = calculateDiscountValue(price);
+  let discount = calculateDiscountPercentage(price);
+
+  return {
+    products,
+    promotion,
+    totalPrice,
+    discountValue,
+    discount,
+  };
 }
 
-module.exports = { getShoppingCart };
+// Busca os elementos do array na base de dados
+function getProductsFromCart(ids, productsList) {
+  const products = ids.map((id) =>
+    productsList.find((product) => id === product.id)
+  );
+  return products;
+}
+
+// Formata os produtos para o objeto de saída
+function getProducts(productsCart) {
+  let products = productsCart.map((product) => ({
+    name: product.name,
+    category: product.category,
+  }));
+  return products;
+}
+
+// Define qual promoção ser aplicada no carrinho
+function setPromotion(products) {
+  const list = products.map((produto) => produto.category);
+  const categories = list.filter(
+    (product, index) => list.indexOf(product) === index
+  );
+  return promotions[categories.length - 1];
+}
+
+// Retorna o preço total do carinho e o preço promocional
+function calculatePrice(products, promotionToBeApplied) {
+  const priceObject = products.reduce(
+    (priceObj, currentProduct) => {
+      // Soma os preços sem desconto
+      priceObj.regularPrice += currentProduct.regularPrice;
+
+      // Procura se o item é elegível para promoção
+      const prom = currentProduct.promotions.find((promotion) =>
+        promotion.looks.includes(promotionToBeApplied)
+      );
+
+      // Se o desconto não existir soma o regularPrice ao totalPrice
+      prom
+        ? (priceObj.totalPrice += prom.price)
+        : (priceObj.totalPrice += currentProduct.regularPrice);
+
+      // Formata o número para duas casa decimais
+      priceObj.totalPrice = Math.round(priceObj.totalPrice * 100) / 100;
+      priceObj.regularPrice = Math.round(priceObj.regularPrice * 100) / 100;
+
+      return priceObj;
+    },
+    { regularPrice: 0, totalPrice: 0 }
+  );
+  /* Retorna um obj onde totalPrice é o preço com descontos e regularPrice é o preço
+   sem descontos */
+  return priceObject;
+}
+
+// Calcula o valor do desconto
+function calculateDiscountValue(priceObject) {
+  return (priceObject.regularPrice - priceObject.totalPrice).toFixed(2);
+}
+
+// Calcula a porcentagem do desconto
+function calculateDiscountPercentage(priceObject) {
+  let percentage = (
+    (1 - priceObject.totalPrice / priceObject.regularPrice) *
+    100
+  ).toFixed(2);
+  return `${percentage}%`;
+}
+
+module.exports = {
+  getShoppingCart,
+  getProductsFromCart,
+  setPromotion,
+  calculatePrice,
+  calculateDiscountValue,
+  calculateDiscountPercentage,
+};
--- Quarto Módulo/nodejs-12/test/index.test.js
+++ Quarto Módulo/nodejs-12/test/index.test.js
@@ -1,82 +1,242 @@
-const { products } = require('../src/data/products');
-const { getShoppingCart } = require('../src');
+const { products } = require("../src/data/products");
+const {
+  getShoppingCart,
+  getProductsFromCart,
+  setPromotion,
+  calculatePrice,
+  calculateDiscountValue,
+  calculateDiscountPercentage,
+} = require("../src");
 
+//FULL LOOK
 const exemplo1Mock = {
-	products: [
-		{ name: 'DISNEY CRUELLA© T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'KNIT JOGGING PANTS', category: 'PANTS' },
-		{ name: 'ASYMMETRICAL LEATHER SLIDE HEELS', category: 'SHOES' },
-		{ name: 'SOFT FLAP BACKPACK', category: 'BAGS' }
-	],
-	promotion: 'FULL LOOK',
-	totalPrice: '404.96',
-	discountValue: '75.00',
-	discount: '15.63%'
+  products: [
+    { name: "DISNEY CRUELLA© T-SHIRT", category: "T-SHIRTS" },
+    { name: "KNIT JOGGING PANTS", category: "PANTS" },
+    { name: "ASYMMETRICAL LEATHER SLIDE HEELS", category: "SHOES" },
+    { name: "SOFT FLAP BACKPACK", category: "BAGS" },
+  ],
+  promotion: "FULL LOOK",
+  totalPrice: "404.96",
+  discountValue: "75.00",
+  discount: "15.63%",
 };
 
+//DOUBLE LOOK
 const exemplo2Mock = {
-	products: [
-		{ name: 'RUBBERIZED PRINTED T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'CONTRAST SLOGAN T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'KNIT JOGGING PANTS', category: 'PANTS' },
-		{ name: 'MENSWEAR PANTS', category: 'PANTS' }
-	],
-		promotion: 'DOUBLE LOOK',
-	totalPrice: '504.95',
-	discountValue: '25.00',
-	discount: '4.72%'
+  products: [
+    { name: "RUBBERIZED PRINTED T-SHIRT", category: "T-SHIRTS" },
+    { name: "CONTRAST SLOGAN T-SHIRT", category: "T-SHIRTS" },
+    { name: "KNIT JOGGING PANTS", category: "PANTS" },
+    { name: "MENSWEAR PANTS", category: "PANTS" },
+  ],
+  promotion: "DOUBLE LOOK",
+  totalPrice: "504.95",
+  discountValue: "25.00",
+  discount: "4.72%",
 };
 
+//SINGLE LOOK
 const exemplo3Mock = {
-	products: [
-		{ name: 'PINK PANTHER™ T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'DISNEY CRUELLA© T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'RUBBERIZED PRINTED T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'CONTRAST SLOGAN T-SHIRT', category: 'T-SHIRTS' }
-	],
-	promotion: 'SINGLE LOOK',
-	totalPrice: '524.96',
-	discountValue: '10.00',
-	discount: '1.87%'
+  products: [
+    { name: "PINK PANTHER™ T-SHIRT", category: "T-SHIRTS" },
+    { name: "DISNEY CRUELLA© T-SHIRT", category: "T-SHIRTS" },
+    { name: "RUBBERIZED PRINTED T-SHIRT", category: "T-SHIRTS" },
+    { name: "CONTRAST SLOGAN T-SHIRT", category: "T-SHIRTS" },
+  ],
+  promotion: "SINGLE LOOK",
+  totalPrice: "524.96",
+  discountValue: "10.00",
+  discount: "1.87%",
 };
 
+//TRIPLE LOOK
 const exemplo4Mock = {
-	products: [
-		{ name: 'PINK PANTHER™ T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'RUBBERIZED PRINTED T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'CONTRAST SLOGAN T-SHIRT', category: 'T-SHIRTS' },
-		{ name: 'KNIT JOGGING PANTS', category: 'PANTS' },
-		{ name: 'ASYMMETRICAL LEATHER SLIDE HEELS', category: 'SHOES' },
-		{ name: 'SLINGBACK KITTEN HEEL SHOES WITH METAL DETAIL', category: 'SHOES' }
-	],
-	promotion: 'TRIPLE LOOK',
-	totalPrice: '784.94',
-	discountValue: '130.00',
-	discount: '14.21%'
+  products: [
+    { name: "PINK PANTHER™ T-SHIRT", category: "T-SHIRTS" },
+    { name: "RUBBERIZED PRINTED T-SHIRT", category: "T-SHIRTS" },
+    { name: "CONTRAST SLOGAN T-SHIRT", category: "T-SHIRTS" },
+    { name: "KNIT JOGGING PANTS", category: "PANTS" },
+    { name: "ASYMMETRICAL LEATHER SLIDE HEELS", category: "SHOES" },
+    {
+      name: "SLINGBACK KITTEN HEEL SHOES WITH METAL DETAIL",
+      category: "SHOES",
+    },
+  ],
+  promotion: "TRIPLE LOOK",
+  totalPrice: "784.94",
+  discountValue: "130.00",
+  discount: "14.21%",
 };
 
-describe('Get Shopping Cart', () => {
-	it('Deve retornar um carrinho de compras a partir do array de IDs do exemplo 1', () => {
-		const cart = getShoppingCart([120, 230, 310, 490], products);
+// [110, 120, 130, 140]
+const productsSingle = [products[0], products[1], products[2], products[3]];
 
-		expect(cart).toEqual(exemplo1Mock);
-	});
+// [130, 140, 230, 260]
+const productsDouble = [products[2], products[3], products[9], products[12]];
 
-	it('Deve retornar um carrinho de compras a partir do array de IDs do exemplo 2', () => {
-		const cart = getShoppingCart([130, 140, 230, 260], products);
+// [110, 130, 140, 230, 310, 330]
+const productsTriple = [
+  products[0],
+  products[2],
+  products[3],
+  products[9],
+  products[13],
+  products[15],
+];
 
-		expect(cart).toEqual(exemplo2Mock);
-	});
+// [120, 230, 310, 490]
+const productsFull = [products[1], products[9], products[13], products[24]];
 
-	it('Deve retornar um carrinho de compras a partir do array de IDs do exemplo 3', () => {
-		const cart = getShoppingCart([110, 120, 130, 140], products);
+const singlePriceObject = { regularPrice: 534.96, totalPrice: 524.96 };
 
-		expect(cart).toEqual(exemplo3Mock);
-	});
+const doublePriceObject = { regularPrice: 529.95, totalPrice: 504.95 };
 
-	it('Deve retornar um carrinho de compras a partir do array de IDs do exemplo 4', () => {
-		const cart = getShoppingCart([110, 130, 140, 230, 310, 330], products);
+const triplePriceObject = { regularPrice: 914.94, totalPrice: 784.94 };
 
-		expect(cart).toEqual(exemplo4Mock);
-	});
+const fullPriceObject = { regularPrice: 479.96, totalPrice: 404.96 };
+
+describe("Get Shopping Cart", () => {
+  it("Deve retornar um carrinho de compras a partir do array de IDs do exemplo 1", () => {
+    const cart = getShoppingCart([120, 230, 310, 490], products);
+
+    expect(cart).toEqual(exemplo1Mock);
+  });
+
+  it("Deve retornar um carrinho de compras a partir do array de IDs do exemplo 2", () => {
+    const cart = getShoppingCart([130, 140, 230, 260], products);
+
+    expect(cart).toEqual(exemplo2Mock);
+  });
+
+  it("Deve retornar um carrinho de compras a partir do array de IDs do exemplo 3", () => {
+    const cart = getShoppingCart([110, 120, 130, 140], products);
+
+    expect(cart).toEqual(exemplo3Mock);
+  });
+
+  it("Deve retornar um carrinho de compras a partir do array de IDs do exemplo 4", () => {
+    const cart = getShoppingCart([110, 130, 140, 230, 310, 330], products);
+
+    expect(cart).toEqual(exemplo4Mock);
+  });
+});
+
+describe("Get products from cart by id array", () => {
+  it("Should get the products from the database with the corresponding id", () => {
+    const productsArray = getProductsFromCart([110, 140, 130, 120], products);
+
+    expect(productsArray.length).toBe(4);
+    expect(productsArray[0].name).toBe("PINK PANTHER™ T-SHIRT");
+    expect(productsArray[3].name).toBe("DISNEY CRUELLA© T-SHIRT");
+    expect(productsArray[1]).toEqual(products[3]);
+  });
+});
+
+describe("Set the promotion  type", () => {
+  it("Should return SINGLE LOOK", () => {
+    // Single Look Promotion
+    const promotionS = setPromotion(productsSingle);
+    expect(promotionS).toBe("SINGLE LOOK");
+  });
+
+  it("Should return DOUBLE LOOK", () => {
+    /// Double Look Promotion
+    const promotionD = setPromotion(productsDouble);
+    expect(promotionD).toBe("DOUBLE LOOK");
+  });
+
+  it("Should return TRIPLE LOOK", () => {
+    /// Triple Look Promotion
+    const promotionT = setPromotion(productsTriple);
+    expect(promotionT).toBe("TRIPLE LOOK");
+  });
+
+  it("Should return FULL LOOK", () => {
+    // Full Look Promotion
+    const promotionF = setPromotion(productsFull);
+    expect(promotionF).toBe("FULL LOOK");
+  });
+});
+
+describe("Calculates the total price", () => {
+  it("Should return the total and promotion price for a single look promotion", () => {
+    const priceObject = calculatePrice(productsSingle, "SINGLE LOOK");
+
+    expect(priceObject.regularPrice).toBe(534.96);
+    expect(priceObject.totalPrice).toBe(524.96);
+  });
+
+  it("Should return the total and promotion price for a double look promotion", () => {
+    const priceObject = calculatePrice(productsDouble, "DOUBLE LOOK");
+
+    expect(priceObject.regularPrice).toBe(529.95);
+    expect(priceObject.totalPrice).toBe(504.95);
+  });
+
+  it("Should return the total and promotion price for a triple look promotion", () => {
+    const priceObject = calculatePrice(productsTriple, "TRIPLE LOOK");
+
+    expect(priceObject.regularPrice).toBe(914.94);
+    expect(priceObject.totalPrice).toBe(784.94);
+  });
+
+  it("Should return the total and promotion price for a full look promotion", () => {
+    const priceObject = calculatePrice(productsFull, "FULL LOOK");
+
+    expect(priceObject.regularPrice).toBe(479.96);
+    expect(priceObject.totalPrice).toBe(404.96);
+  });
+});
+
+describe("Calculates the discount value", () => {
+  it("Should calculate the discount value for the single look", () => {
+    const discount = calculateDiscountValue(singlePriceObject);
+    const value = 10;
+    expect(discount).toBe(value.toFixed(2));
+  });
+
+  it("Should calculate the discount value for the double look", () => {
+    const discount = calculateDiscountValue(doublePriceObject);
+    const value = 25;
+    expect(discount).toBe(value.toFixed(2));
+  });
+
+  it("Should calculate the discount value for the triple look", () => {
+    const discount = calculateDiscountValue(triplePriceObject);
+    const value = 130;
+    expect(discount).toBe(value.toFixed(2));
+  });
+
+  it("Should calculate the discount value for the full look", () => {
+    const discount = calculateDiscountValue(fullPriceObject);
+    const value = 75;
+    expect(discount).toBe(value.toFixed(2));
+  });
+});
+
+describe("Calculates the percentage", () => {
+  it("Should return the discount percentage value for the single look promotion", () => {
+    const percentage = calculateDiscountPercentage(singlePriceObject);
+
+    expect(percentage).toBe("1.87%");
+  });
+
+  it("Should return the discount percentage value for the double look promotion", () => {
+    const percentage = calculateDiscountPercentage(doublePriceObject);
+
+    expect(percentage).toBe("4.72%");
+  });
+
+  it("Should return the discount percentage value for the triple look promotion", () => {
+    const percentage = calculateDiscountPercentage(triplePriceObject);
+
+    expect(percentage).toBe("14.21%");
+  });
+
+  it("Should return the discount percentage value for the full look promotion", () => {
+    const percentage = calculateDiscountPercentage(fullPriceObject);
+
+    expect(percentage).toBe("15.63%");
+  });
 });
