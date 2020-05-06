d9d73547c9067116507e5e112a4f07dc3e1246f0
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-06 13:03:19 -0300

    Commit inicial do projeto React Ricky and Morty

---
+++ Quinto Módulo/Aulas/aula-ricky-morty/projeto-aula-ricky-morty/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button name="Todos" />
+        <Button name="Vivo" />
+        <Button name="Morto" />
+        <Button name="Desconhecido" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
