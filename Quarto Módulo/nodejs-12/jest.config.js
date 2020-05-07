03b3d53ab67fda4ccc2ee0b7514ac59a4d711faf
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-07 15:35:26 -0300

    Commit inicial

--- Quarto Módulo/nodejs-12/jest.config.js
+++ Quarto Módulo/nodejs-12/jest.config.js
@@ -1,7 +1,10 @@
-// module.exports = {
-//     reporters: [
-//       [ 'jest-junit', {
-//         outputName: 'output.xml',
-//       } ]
-//     ]
-//   };
+module.exports = {
+  reporters: [
+    [
+      "jest-junit",
+      {
+        outputName: "output.xml",
+      },
+    ],
+  ],
+};
