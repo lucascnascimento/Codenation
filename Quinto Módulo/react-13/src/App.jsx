3dedb0f0af8346ec97eb7f7cc147f75075d88e45
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-07 21:07:07 -0300

    Estrututação do app.jsx

--- Quinto Módulo/react-13/src/App.jsx
+++ Quinto Módulo/react-13/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Topbar from "./components/Topbar";
+import Filters from "./components/Filters";
+import Contacts from "./components/Contacts";
 
 import "./App.scss";
 
@@ -9,56 +11,8 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Topbar />
-
-        <div className="container">
-          <section className="filters">
-            <div className="filters__search">
-              <input
-                type="text"
-                className="filters__search__input"
-                placeholder="Pesquisar"
-              />
-
-              <button className="filters__search__icon">
-                <i className="fa fa-search" />
-              </button>
-            </div>
-
-            <button className="filters__item is-selected">
-              Nome <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              País <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              Empresa <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              Departamento <i className="fas fa-sort-down" />
-            </button>
-
-            <button className="filters__item">
-              Data de admissão <i className="fas fa-sort-down" />
-            </button>
-          </section>
-        </div>
-
-        <div className="container">
-          <section className="contacts">
-            <article className="contact">
-              <span className="contact__avatar" />
-              <span className="contact__data">Nome</span>
-              <span className="contact__data">Telefone</span>
-              <span className="contact__data">País</span>
-              <span className="contact__data">Admissão</span>
-              <span className="contact__data">Empresa</span>
-              <span className="contact__data">Departamento</span>
-            </article>
-          </section>
-        </div>
+        <Filters />
+        <Contacts />
       </React.Fragment>
     );
   }
