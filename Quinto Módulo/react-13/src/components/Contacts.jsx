32a22f30101b2ddc09094b0a3a6e2429a8907c8e
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-07 21:06:44 -0300

    Criação do componente de contatos

--- Quinto Módulo/react-13/src/components/Contacts.jsx
+++ Quinto Módulo/react-13/src/components/Contacts.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import Contact from "./Contact";
+
 class Contacts extends React.Component {
-	render() {
-		return (<div />);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <section className="contacts">
+            <Contact />
+          </section>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Contacts;
