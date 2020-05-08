fd094f2995e054a574948a56506fc9e3b1c74912
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-08 10:31:53 -0300

    Criação do componentDidMount com a fetch api

--- Quinto Módulo/react-13/src/App.jsx
+++ Quinto Módulo/react-13/src/App.jsx
@@ -7,6 +7,25 @@ import Contacts from "./components/Contacts";
 import "./App.scss";
 
 class App extends React.Component {
+  // Pega os dados da API
+  componentDidMount() {
+    fetch("https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts")
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log("ERROR: " + response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  }
+
   render() {
     return (
       <React.Fragment>
