a5b5a22db2a805672f6e27d9522850e0e8f1f127
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-12 12:04:59 -0300

    Configuração inicial

--- Sexto Módulo/react-14/src/components/Contact.jsx
+++ Sexto Módulo/react-14/src/components/Contact.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from "react";
 
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    const {
+      avatar,
+      name,
+      phone,
+      country,
+      admissionDate,
+      company,
+      department,
+    } = this.props.data;
+
+    return (
+      <React.Fragment>
+        <article className="contact" data-testid="contact">
+          <div className="contact__avatar">
+            <img src={avatar} alt="avatar" />
+          </div>
+          <span className="contact__data">{name}</span>
+          <span className="contact__data">{phone}</span>
+          <span className="contact__data">{country}</span>
+          <span className="contact__data">{admissionDate}</span>
+          <span className="contact__data">{company}</span>
+          <span className="contact__data">{department}</span>
+        </article>
+      </React.Fragment>
+    );
   }
 }
 
--- Sexto Módulo/react-14/src/components/Contacts.jsx
+++ Sexto Módulo/react-14/src/components/Contacts.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 class Contacts extends React.Component {
-	render() {
-		return (<div />);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container" data-testid="contacts">
+          <section className="contacts">
+            <article className="contact">
+              <span className="contact__avatar" />
+              <span className="contact__data">Nome</span>
+              <span className="contact__data">Telefone</span>
+              <span className="contact__data">País</span>
+              <span className="contact__data">Admissão</span>
+              <span className="contact__data">Empresa</span>
+              <span className="contact__data">Departamento</span>
+            </article>
+          </section>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Contacts;
