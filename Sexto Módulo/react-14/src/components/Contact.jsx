be40eb757431c11cd7cf181a39c9d73f9e55e427
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-14 10:48:36 -0300

    Novo método para formatar a data

--- Sexto Módulo/react-14/src/components/Contact.jsx
+++ Sexto Módulo/react-14/src/components/Contact.jsx
@@ -12,20 +12,6 @@ class Contact extends React.Component {
       department,
     } = this.props.data;
 
-    const formatDate = (admissionDate) => {
-      const date = new Date(admissionDate);
-      let dd = date.getDate();
-      let mm = date.getMonth() + 1;
-      let yyyy = date.getFullYear();
-      if (dd < 10) {
-        dd = "0" + dd;
-      }
-      if (mm < 10) {
-        mm = "0" + mm;
-      }
-      return dd + "/" + mm + "/" + yyyy;
-    };
-
     return (
       <article className="contact container" data-testid="contact">
         <div className="contact__avatar">
@@ -41,7 +27,7 @@ class Contact extends React.Component {
           {country}
         </span>
         <span className="contact__data" data-testid="contact-date">
-          {formatDate(admissionDate)}
+          {new Date(admissionDate).toLocaleDateString("pt-BR")}
         </span>
         <span className="contact__data" data-testid="contact-company">
           {company}
