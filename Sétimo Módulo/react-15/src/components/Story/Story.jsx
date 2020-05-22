d541f34b98e001ae20916d70fdeeeac1133b5697
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-05-22 09:11:53 -0300

    Criação do Story e dos Stories

--- Sétimo Módulo/react-15/src/components/Story/Story.jsx
+++ Sétimo Módulo/react-15/src/components/Story/Story.jsx
@@ -1,13 +1,42 @@
 import React, { useCallback, useState } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import './Story.scss';
+import "./Story.scss";
 
 const Story = ({ story, user, handleClose }) => {
   return (
-    <section className="story">
-      <div className="container"></div>
+    <section className="story" data-testid="story">
+      <div className="container">
+        <header className="story__header">
+          <div className="user">
+            <Link to="/users/blackpanther">
+              <img
+                className="user__thumb"
+                src="https://viniciusvinna.netlify.app/assets//api-instagram/profiles/black-panther/black-panther-profile.jpg"
+                alt="T'Challa"
+              />
+            </Link>
+            <Link to="/users/blackpanther" className="user__name">
+              T'Challa
+            </Link>
+          </div>
+          <button className="story__close" onClick={handleClose}>
+            <i class="fas fa-times"></i>
+          </button>
+        </header>
+
+        <div className="story__progress">
+          <div className="story__progress__elapsed"></div>
+        </div>
+
+        <div className="story__video__wrapper ">
+          <video
+            className="video-player"
+            src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/black-panther/black-panther-stories.mp4"
+          ></video>
+        </div>
+      </div>
     </section>
   );
 };
--- Sétimo Módulo/react-15/src/containers/Stories/Stories.jsx
+++ Sétimo Módulo/react-15/src/containers/Stories/Stories.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 
-import Story from '../../components/Story';
+import Story from "../../components/Story";
 
-import './Stories.scss';
+import "./Stories.scss";
 
 const Stories = ({ stories, getUserHandler }) => {
+  const [showStory, setShowStory] = useState(false);
+
   return (
     <React.Fragment>
-      <section className="stories">
+      <section className="stories" data-testid="stories">
         <div className="container">
+          <button
+            className="user__thumb"
+            key="t'challa"
+            onClick={() => setShowStory(!showStory)}
+          >
+            <div className="user__thumb__wrapper">
+              <img
+                src="https://viniciusvinna.netlify.app/assets//api-instagram/profiles/black-panther/black-panther-profile.jpg"
+                alt="story"
+              />
+            </div>
+          </button>
         </div>
       </section>
 
       {showStory && (
-        <Story />
-        )}
+        <Story
+          story="https://5e7d0266a917d70016684219.mockapi.io/api/v1/stories/1"
+          user="https://5e7d0266a917d70016684219.mockapi.io/api/v1/users/1"
+          handleClose={() => setShowStory(!showStory)}
+        />
+      )}
     </React.Fragment>
   );
 };
