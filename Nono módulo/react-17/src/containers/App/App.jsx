6703bc0ccceadbe7dbb063ade85f1d225b9a5796
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-12 10:25:40 -0300

    App

--- Nono módulo/react-17/src/containers/App/App.jsx
+++ Nono módulo/react-17/src/containers/App/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
-import './App.scss';
+import Routes from "../../routes";
 
-const App = () => (<div className="app" />);
+import { store, persistor } from "../../store";
+
+import "./App.scss";
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <div className="app" data-testid="app">
+        <Router>
+          <Routes />
+        </Router>
+      </div>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
