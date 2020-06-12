76214ced02e4c99214eb733266d2fe071c283844
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-12 08:54:38 -0300

    Criação do store

---
+++ Nono módulo/react-17/src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import rootReducer from "../reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["auth", "content"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
